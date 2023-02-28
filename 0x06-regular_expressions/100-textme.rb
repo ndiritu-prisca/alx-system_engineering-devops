f710b8907328bcea6296b81f1cf1e83311470516
ndiritu-prisca <priscandiritu@outlook.com>
2023-02-28 22:17:34 +0000

    Trials

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 #puts ARGV[0].scan(/.*from:([^\]]+).*to:([\+\d]+).*flags:([-\d:]+).*/).join
 match = ARGV[0].match(/.*from:([^\]]+).*to:([\+\d]+).*flags:([-\d:]+).*/)
+#match = ARGV[0].match(/.*from:(\D?[\w]*).*to:(\D\d*).*flags:([\D+\d]{12}).*/)
 output = "#{match[1]},#{match[2]},#{match[3]}"
 puts output
