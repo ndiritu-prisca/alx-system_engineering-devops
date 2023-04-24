934b86d514b0a482bd6983a6e2cf2cf6ef6d8ded
ndiritu-prisca <priscandiritu@outlook.com>
2023-04-24 20:44:17 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# Using Puppet set up your client SSH configuration file so that you can
+# connect to a server without typing a password.
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
