97c2341940d9a0cabd2f772b3845a3d06823a2da
ndiritu-prisca <priscandiritu@outlook.com>
2023-05-23 07:42:45 +0000

    Corrections on list of dictionary

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,14 +9,12 @@ import requests
 from sys import argv
 
 
-def json_export(employee_id):
+def json_export():
     """Function to export data in JSON format"""
     # Fetch employee details
     url = "https://jsonplaceholder.typicode.com/"
     employee_url = "{}users".format(url)
     employee_data = requests.get(employee_url).json()
-    employee_name = employee_data.get("name")
-    user_name = employee_data.get("username")
 
     # Fetch employee's todos
     todos_url = "{}todos".format(url)
@@ -24,20 +22,18 @@ def json_export(employee_id):
 
     # Export in JSON formart
     my_dict = {}
-    username_dict = {}
 
-    for info in employee_data:
-        user_id = info.get('id')
-        my_dict[user_id] = []
-        username_dict[user_id] = info.get("username")
-
-    for info in tasks_dict:
-        all_tasks = {}
-        user_id = info.get("userId")
-        all_tasks["task"] = info.get("title")
-        all_tasks["completed"] = info.get("completed")
-        all_tasks["username"] = username_dict.get(user_id)
-        my_dict.get(user_id).append(all_tasks)
+    for employee in employee_data:
+        all_tasks = []
+        for info in tasks_dict:
+            task = {
+                "task" = info.get("title")
+                "completed" = info.get("completed")
+                "username" = employee.get("username")
+            }
+            all_tasks.append(task)
+        user_id = employee.get("id")
+        my_dict[user_id] = all_tasks
 
     file_name = "todo_all_employees.json"
     with open(file_name, mode='w') as json_file:
@@ -45,4 +41,4 @@ def json_export(employee_id):
 
 
 if __name__ == "__main__":
-    json_export(int(argv[1]))
+    json_export()
