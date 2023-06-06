4932c7b1b9742da03ac921ce60a4d070bd7fa532
ndiritu-prisca <priscandiritu@outlook.com>
2023-06-06 15:07:23 +0000

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    elif response.status_code == 404:
+        return 0
+    else:
+        return 0
