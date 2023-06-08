af282af012ed3ccf6bcdf8f29607aa560f5cd2d2
ndiritu-prisca <priscandiritu@outlook.com>
2023-06-08 14:15:53 +0000

    Recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+
 import requests
+
+
 """
     Recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit.
@@ -11,9 +15,9 @@ def recurse(subreddit, hot_list=[]):
     url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
     headers = {"User-Agent": "Custom User Agent"}
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        data = resp.json()
         posts = data["data"]["children"]
         for post in posts:
             title = post["data"]["title"]
