6e11624e8f0f11cfb89a479d6b16a850b3c61178
ndiritu-prisca <priscandiritu@outlook.com>
2023-06-08 14:19:39 +0000

    Placement of documentation

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-import requests
-
-
 """
     Recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit.
     If no results are found for the given subreddit, the function should
     return None.
 """
+
+import requests
+
 
 def recurse(subreddit, hot_list=[]):
     """The recursive function that queries Reddit API"""
